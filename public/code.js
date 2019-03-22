70733e8092d6fc7741dd1f6610552349f1d9d334
Chris <43723144+Chrisjnovell@users.noreply.github.com>
2019-03-22 14:35:34 +0000

    Add files via upload
    Basics for the server and corresponding json files

--- public/code.js
+++ public/code.js
@@ -1,1 +1,13 @@
-// JavaScript file for Bake-Away
+// JavaScript file for Bake-Away Server
+'use strict'
+const express = require("express")
+const app = express()
+const port = 8080;
+
+app.use(express.static("public"))
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" )
+})
+
+app.listen(port, () => console.log("listening"))
