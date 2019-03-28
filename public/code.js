278fe7c5fd4edb9231207923aa0a4959530438ad
Chris <chrisjnovell@gmail.com>
2019-03-28 23:05:16 +0000

    Changed path for all images in all html files

--- public/code.js
+++ public/code.js
@@ -10,10 +10,4 @@ const con = mysql.createConnection(config.mysql)
 app.use(express.static("images"))
 app.use(express.static("webpages"))
 
-/*
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/" + "default.css")
-  res.sendFile(__dirname + "/" + "index.html")
-})
-*/
 app.listen(port, () => console.log("server is listening"))
