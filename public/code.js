1d1cf30d8cb8e4ab84ac53826be5e59ed870f740
Chris <chrisjnovell@gmail.com>
2019-03-28 22:47:38 +0000

    Changed repos, made server load html + css

--- public/code.js
+++ public/code.js
@@ -7,10 +7,13 @@ const mysql = require('mysql')
 const config = require('./config.js')
 const con = mysql.createConnection(config.mysql)
 
-app.use(express.static("public"))
+app.use(express.static("images"))
+app.use(express.static("webpages"))
 
+/*
 app.get('/', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.html" )
+  res.sendFile(__dirname + "/" + "default.css")
+  res.sendFile(__dirname + "/" + "index.html")
 })
-
-app.listen(port, () => console.log("listening"))
+*/
+app.listen(port, () => console.log("server is listening"))
